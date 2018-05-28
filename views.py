7e9ac6efd02a4dda8b2c899b1d9f9f7c48535b39
Andrey Bezpalov <abezpalov@ya.ru>
2018-05-28 19:02:32 +0400

    template.article

--- views.py
+++ views.py
@@ -1,8 +1,28 @@
+from django.conf import settings
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from django.http import HttpResponse # without templates
+
+from .models import *
+
 
 def index(request):
 
+    site_name = settings.SITE_NAME
+
+    # TODO
     content = 'index test content'
 
     return render(request, 'content/index.html', locals())
+
+
+def article_by_alias(request, alias):
+
+    site_name = settings.SITE_NAME
+
+    try:
+        article = Article.objects.get(alias = alias)
+    except Article.DoesNotExist:
+        raise Http404('Ошибка! Статья не найдена.')
+
+    return render(request, 'content/article.html', locals())
